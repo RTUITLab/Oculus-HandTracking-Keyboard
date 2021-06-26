6d3454d14b69698db1e98284f86ee0a67b267fe8
Eloren1 <elorenn@bk.ru>
2021-06-26 15:23:55 +0300

    Display typed text

---
+++ Assets/Scripts/Display.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Display : MonoBehaviour
+{
+    [SerializeField] private InputField inputField;
+    [SerializeField] private Keyboard keyboard;
+
+    private void Update()
+    {
+        inputField.text = keyboard.Text;
+    }
+}
